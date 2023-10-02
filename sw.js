a2084f6935824bf771799496b4b31b05689f07e8
Edea1992 <el295ipqb0lj86aq@gmail.com>
2023-10-02 16:01:39 +0900

    a

--- sw.js
+++ sw.js
@@ -44,30 +44,107 @@ const regex = /^https:\/\/pilipili\.com\/video\/(.*)/
 //     }
 // })
 
-self.addEventListener("fetch",async (event) => {
+self.addEventListener("fetch", (event) => {
     const matches = event.request.url.match(regex)
     if (matches) {
-        event.respondWith(async () => {
-            const response = await fetch(`https://gist.githubusercontent.com/Edea1992/${matches[1]}`)
-            const bytes = new Uint8Array([...atob(await response.text())].map(char => char.charCodeAt(0)))
+        event.respondWith((async () => {
+            const url = `https://gist.githubusercontent.com/Edea1992/${matches[1]}`
+
+            if (url.endsWith("m3u8")) {
+                const m3u8 = await (await fetch(url)).text()
+                
+                const range = event.request.headers.get("Content-Range")
+                if (range) {
+                    const parts = range.replace("bytes=", "").split("-")
+                    const start = parseInt(parts[0], 10)
+                    const end = parts[1] ? parseInt(parts[1], 10) : m3u8.length - 1
+                    const chunkSize = end - start + 1
+                    const chunk = m3u8.slice(start, end + 1)
+                    return new Response(
+                        new ReadableStream({
+                            start(controller) {
+                                controller.enqueue(chunk)
+                                controller.close()
+                            }
+                        }),
+                        {
+                            headers: {
+                                "Accept-Ranges":  "bytes",
+                                "Content-Length": chunkSize,
+                                "Content-Range":  `bytes ${start}-${end}/${m3u8.length}`,
+                                "Content-Type":  "application/x-mpegURL"
+                            },
+                            status: 206
+                        }
+                    )
+                }
+
+                return new Response(
+                    new ReadableStream({
+                        start(controller) {
+                            controller.enqueue(m3u8)
+                            controller.close()
+                        }
+                    }),
+                    {
+                        headers: {
+                            "Accept-Ranges":  "bytes",
+                            "Content-Length": m3u8.length,
+                            "Content-Type":  "application/x-mpegURL"
+                        }
+                    }
+                )
+            }
+
+            const data = new Uint8Array([...atob(await (await fetch(url)).text())].map(char => char.charCodeAt(0)))
+            
+            const range = event.request.headers.get("Content-Range")
+            if (range) {
+                const parts = range.replace("bytes=", "").split("-")
+                const start = parseInt(parts[0], 10)
+                const end = parts[1] ? parseInt(parts[1], 10) : data.length - 1
+                const chunkSize = end - start + 1
+                const chunk = data.slice(start, end + 1)
+                return new Response(
+                    new ReadableStream({
+                        start(controller) {
+                            controller.enqueue(chunk)
+                            controller.close()
+                        }
+                    }),
+                    {
+                        headers: {
+                            "Accept-Ranges":  "bytes",
+                            "Content-Length": chunkSize,
+                            "Content-Range":  `bytes ${start}-${end}/${data.length}`,
+                            "Content-Type":  "video/mp2t"
+                        },
+                        status: 206
+                    }
+                )
+            }
 
             return new Response(
                 new ReadableStream({
                     start(controller) {
-                        controller.enqueue(bytes)
+                        controller.enqueue(data)
                         controller.close()
                     }
                 }),
                 {
                     headers: {
                         "Accept-Ranges":  "bytes",
-                        "Content-Length": bytes.length,
+                        "Content-Length": data.length,
                         "Content-Type":  "video/mp2t"
                     }
                 }
             )
-        })
+        })())
     } else {
-        event.respondWith(fetch(event.request))
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                return response || fetch(event.request)
+            })
+        )
     }
 })
